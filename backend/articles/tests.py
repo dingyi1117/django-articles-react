955ca7d1d01f3f5a4bd892d3ec1662079ea080cf
dingyi1117 <dingyi.yy@gmail.com>
2019-06-13 04:59:17 -0400

    delete mysql dependency conflict

--- backend/articles/tests.py
+++ backend/articles/tests.py
@@ -2,6 +2,9 @@ from django.test import TestCase
 
 from .models import Article
 
+#######################
+# need to be done
+#########################
 class ArticleModelTests(TestCase):
     def test_wrong_date(self):
         article = Article(created_at = "2020-11-11")
