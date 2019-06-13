011e8beb3eafad6bf6c2ff1c744823eda7266342
dingyi1117 <dingyi.yy@gmail.com>
2019-06-13 04:07:54 -0400

    create test case

--- backend/articles/tests.py
+++ backend/articles/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Article
+
+class ArticleModelTests(TestCase):
+    def test_wrong_date(self):
+        article = Article(created_at = "2020-11-11")
+self.assertIs()
